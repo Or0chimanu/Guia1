de8fc507534502591c241d443d32aab937cd3037
Manuel Obaya <mobaya@itba.edu.ar>
2026-03-11 21:40:15 -0300

    Se termino el ej 9. Correcta implementacion de listas y sigint

--- ej_9.c
+++ ej_9.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
+#include "lists.h"
+#include <windows.h>
+
+lista *nombres = NULL;
 
 int verificar_largo(char string[]){
     if (strchr(string, '\n') == NULL)
@@ -13,6 +18,21 @@ int verificar_largo(char string[]){
             return 0;
         }
     return 1;
+
+}
+
+void handler(int sig){
+    char buffer[100];
+    int i = 0;
+    int n = size_lista(nombres);
+    printf("Los nombres incorrectos ingresados fueron:\n");
+    if(n>0){
+        for(i = 0;i<n;i++){
+            pop(nombres,buffer,100,0);
+            printf("%s",buffer);
+        }
+    }
+    exit(0);
 
 }
 
@@ -43,18 +63,25 @@ void upper(char *string){
 int main(void)
 {
     char nombre[10];
-
-
     nombre[0] = '\0';
+
+    signal(SIGINT, handler);
+
     while (nombre[0] == '\0')
     {
         printf("Ingrese su nombre: ");
         fgets(nombre, 10, stdin);
         if(verificar_largo(nombre) == 0){puts("El nombre que ingreso es demasiado largo");nombre[0]='\0';continue;}
-        if(verificar_string(nombre) == 0){puts("El nombre contiene caracteres especiales");nombre[0]='\0';continue;}
+        if(verificar_string(nombre) == 0){puts("El nombre contiene caracteres especiales");nombres = push(nombres,nombre);nombre[0]='\0';continue;}
         nombre[sizeof(nombre)- 1] = '\0';
         upper(nombre);
         printf("El nombre correctamente ingresado es %s \n",nombre);
     }
 
+    while(1) {
+        Sleep(1);
+    }
+
+
+    return 0;
 }
